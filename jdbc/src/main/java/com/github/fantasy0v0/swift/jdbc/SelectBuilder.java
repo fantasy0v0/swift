b628c9c3698c1d01ccfcd46dc2d245ec729c6175
fan <xiaofanms@outlook.com>
2023-12-22 14:10:41 +0800

    完成fetchOne。

--- jdbc/src/main/java/com/github/fantasy0v0/swift/jdbc/SelectBuilder.java
+++ jdbc/src/main/java/com/github/fantasy0v0/swift/jdbc/SelectBuilder.java
@@ -1,7 +1,6 @@
 package com.github.fantasy0v0.swift.jdbc;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -23,41 +22,46 @@ public class SelectBuilder {
     return new PagingBuilder(dataSource, sql, params);
   }
 
-  private <T> List<T> _fetch(FetchMapper<T> mapper, ParameterProcess parameterProcess) throws SQLException {
-    try (Connection conn = Utils.getConnection(dataSource)) {
-      return Utils.executeQuery(conn, sql, params, parameterProcess, mapper);
-    }
-  }
-
   public <T> List<T> fetch(FetchMapper<T> mapper, ParameterProcess parameterProcess) {
     try {
-      return _fetch(mapper, parameterProcess);
+      return Utils._fetch(dataSource, sql, params, mapper, parameterProcess, false);
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
 
   public <T> List<T> fetch(FetchMapper<T> mapper) {
+    return fetch(mapper, null);
+  }
+
+  private Object[] getRowByObject(Row row) throws SQLException {
+    int columnCount = row.getColumnCount();
+    Object[] array = new Object[columnCount];
+    for (int i = 0; i < columnCount; i++) {
+      array[i] = row.getObject(i + 1);
+    }
+    return array;
+  }
+
+  public List<Object[]> fetch() {
+    return fetch(this::getRowByObject);
+  }
+
+  public <T> T fetchOne(FetchMapper<T> mapper, ParameterProcess parameterProcess) {
     try {
-      return fetch(mapper, null);
+      List<T> list = Utils._fetch(dataSource, sql, params, mapper, parameterProcess, true);
+      return list.isEmpty() ? null : list.getFirst();
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
 
-  public List<Object[]> fetch() {
-    return fetch(row -> {
-      int columnCount = row.getColumnCount();
-      Object[] array = new Object[columnCount];
-      for (int i = 0; i < columnCount; i++) {
-        array[i] = row.getObject(i + 1);
-      }
-      return array;
-    });
+  public <T> T fetchOne(FetchMapper<T> mapper) {
+    return fetchOne(mapper, null);
   }
 
-  public <T> T fetchOne(FetchMapper<T> mapper) {
-    return null;
+  public Object[] fetchOne() {
+    return fetchOne(this::getRowByObject);
   }
 
 }
--- jdbc/src/main/java/com/github/fantasy0v0/swift/jdbc/Utils.java
+++ jdbc/src/main/java/com/github/fantasy0v0/swift/jdbc/Utils.java
@@ -17,9 +17,20 @@ final class Utils {
     return dataSource.getConnection();
   }
 
-  static <T> List<T> executeQuery(Connection conn, String sql, List<Object> params,
+  static <T> List<T> _fetch(DataSource dataSource,
+                            String sql, List<Object> params,
+                            FetchMapper<T> mapper, ParameterProcess parameterProcess,
+                            boolean firstOnly) throws SQLException {
+    try (Connection conn = Utils.getConnection(dataSource)) {
+      return executeQuery(conn, sql, params, mapper, parameterProcess, firstOnly);
+    }
+  }
+
+  static <T> List<T> executeQuery(Connection conn,
+                                  String sql, List<Object> params,
+                                  FetchMapper<T> fetchMapper,
                                   ParameterProcess parameterProcess,
-                                  FetchMapper<T> fetchMapper) throws SQLException {
+                                  boolean firstOnly) throws SQLException {
     LogUtil.performance().info("executeQuery begin");
     long startTime = System.nanoTime() / 1000;
     LogUtil.sql().debug("executeQuery: {}", sql);
@@ -27,20 +38,27 @@ final class Utils {
       fillStatementParams(conn, statement, params, parameterProcess);
       try (ResultSet resultSet = statement.executeQuery()) {
         List<T> array = new ArrayList<>();
+        boolean first = true;
         while (resultSet.next()) {
           T row = fetchMapper.apply(new Row(resultSet));
           array.add(row);
+          if (first && firstOnly) {
+            break;
+          }
+          first = false;
         }
         return array;
       }
     } finally {
       long cost = System.nanoTime() / 1000 - startTime;
       NumberFormat format = NumberFormat.getNumberInstance();
-      LogUtil.performance().info("executeQuery end. cost: {} μs", format.format(cost));
+      LogUtil.performance().info("executeQuery end, cost: {} μs", format.format(cost));
     }
   }
 
-  static boolean execute(Connection conn, String sql, List<Object> params, ParameterProcess parameterProcess) throws SQLException {
+  static boolean execute(Connection conn,
+                         String sql, List<Object> params,
+                         ParameterProcess parameterProcess) throws SQLException {
     LogUtil.performance().info("execute begin");
     long startTime = System.nanoTime() / 1000;
     LogUtil.sql().debug("execute: {}", sql);
@@ -50,7 +68,7 @@ final class Utils {
     } finally {
       long cost = System.nanoTime() / 1000 - startTime;
       NumberFormat format = NumberFormat.getNumberInstance();
-      LogUtil.performance().info("execute end. cost: {} μs", format.format(cost));
+      LogUtil.performance().info("execute end, cost: {} μs", format.format(cost));
     }
   }
 
