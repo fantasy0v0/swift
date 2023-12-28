7f294a19ae645ca667f555a7435c6e7ef60b19df
fan <xiaofanms@outlook.com>
2023-12-28 11:18:26 +0800

    增加一些常用的谓词。

--- jdbc/src/main/java/com/github/fantasy0v0/swift/jdbc/SQLDialect.java
+++ jdbc/src/main/java/com/github/fantasy0v0/swift/jdbc/SQLDialect.java
@@ -1,4 +1,39 @@
 package com.github.fantasy0v0.swift.jdbc;
 
+import com.github.fantasy0v0.swift.jdbc.predicate.Predicate;
+
 public interface SQLDialect {
+
+  Predicate equal(String expression, Object... params);
+
+  Predicate notEqual(String expression, Object... params);
+
+  /**
+   * less-than
+   */
+  Predicate lt(String expression, Object... params);
+
+  /**
+   * less-than-or-equal
+   */
+  Predicate le(String expression, Object... params);
+
+  /**
+   * greater-than
+   */
+  Predicate gt(String expression, Object... params);
+
+  /**
+   * greater-than-or-equal
+   */
+  Predicate ge(String expression, Object... params);
+
+  Predicate like(String expression, Object... params);
+
+  Predicate notLike(String expression, Object... params);
+
+  Predicate isNull(String expression, Object... params);
+
+  Predicate isNotNull(String expression, Object... params);
+
 }
