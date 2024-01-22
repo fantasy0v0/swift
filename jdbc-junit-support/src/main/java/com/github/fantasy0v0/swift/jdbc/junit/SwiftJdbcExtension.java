fe2eb5d3dc6e924a03e2a07f7e613fe1aa050d45
fan <xiaofanms@outlook.com>
2024-01-22 14:24:11 +0800

    解决测试环境下连接没被关闭的问题。

--- jdbc-junit-support/src/main/java/com/github/fantasy0v0/swift/jdbc/junit/SwiftJdbcExtension.java
+++ jdbc-junit-support/src/main/java/com/github/fantasy0v0/swift/jdbc/junit/SwiftJdbcExtension.java
@@ -12,10 +12,12 @@ import java.util.Objects;
 
 public class SwiftJdbcExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
 
+  private static final ExtensionContext.Namespace TEST_CONTEXT_NAMESPACE =
+    ExtensionContext.Namespace.create(SwiftJdbcExtension.class);
+
   @Override
   public void afterTestExecution(ExtensionContext extensionContext) throws Exception {
-    DataSource dataSource = Objects.requireNonNull(JDBC.getDataSource(), "未配置dataSource");
-    ConnectionReference ref = ConnectionReference.getReference(dataSource);
+    ConnectionReference ref = getConnectionReference(extensionContext);
     if (null != ref) {
       Connection connection = ref.unwrap();
       connection.rollback();
@@ -29,5 +31,21 @@ public class SwiftJdbcExtension implements BeforeTestExecutionCallback, AfterTes
     ConnectionReference ref = ConnectionReference.getReference(dataSource);
     Connection connection = ref.unwrap();
     connection.setAutoCommit(false);
+    setConnectionReference(extensionContext, ref);
+  }
+
+  void setConnectionReference(ExtensionContext context,
+                              ConnectionReference connectionReference) {
+    ExtensionContext.Store store = getStore(context);
+    store.put("connectionReference", connectionReference);
+  }
+
+  ConnectionReference getConnectionReference(ExtensionContext context) {
+    ExtensionContext.Store store = getStore(context);
+    return (ConnectionReference) store.get("connectionReference");
+  }
+
+  private ExtensionContext.Store getStore(ExtensionContext context) {
+    return context.getRoot().getStore(TEST_CONTEXT_NAMESPACE);
   }
 }
