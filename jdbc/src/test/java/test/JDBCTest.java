4ec1a21b1b479860e94b2150f6130ebbf18b7a40
fan <xiaofanms@outlook.com>
2024-11-20 18:09:47 +0800

    使用新的单元测试方法。

--- jdbc/src/test/java/test/JDBCTest.java
+++ jdbc/src/test/java/test/JDBCTest.java
@@ -4,10 +4,13 @@ import com.github.fantasy0v0.swift.jdbc.JDBC;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import test.container.ContainerUtil;
 import test.container.JdbcContainer;
+import test.container.SwiftJdbcExtension;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -16,27 +19,13 @@ import static com.github.fantasy0v0.swift.jdbc.JDBC.select;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(SwiftJdbcExtension.class)
 class JDBCTest {
 
   private final Logger log = LoggerFactory.getLogger(JDBCTest.class);
 
-  private final static JdbcContainer container = JdbcContainer.create(
-    ContainerUtil.PG, ContainerUtil.PG_LOCATIONS
-  );
-
-  @BeforeAll
-  static void beforeAll() {
-    DataSource dataSource = container.start();
-    JDBC.configuration(dataSource);
-  }
-
-  @AfterAll
-  static void afterAll() {
-    container.stop();
-  }
-
-  @Test
-  void testStatement() {
+  @TestTemplate
+  void testStatement(DataSource dataSource) {
     List<Object[]> data = select("""
       select * from student;
     """).fetch();
