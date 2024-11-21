fc9c757c72548f8f1615c994df27f9c84a8a6221
fan <xiaofanms@outlook.com>
2024-11-21 15:59:07 +0800

    完成#4

--- jdbc/src/test/java/test/container/ContainerUtil.java
+++ jdbc/src/test/java/test/container/ContainerUtil.java
@@ -1,21 +1,11 @@
 package test.container;
 
-import com.github.fantasy0v0.swift.jdbc.JDBC;
-import org.junit.jupiter.api.DynamicTest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-import javax.sql.DataSource;
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.DynamicTest.dynamicTest;
-
 public final class ContainerUtil {
-
-  private static final Logger log = LoggerFactory.getLogger(ContainerUtil.class);
 
   public static final PostgreSQLContainer<?> PG = new PostgreSQLContainer<>("postgres:16-alpine");
 
@@ -29,27 +19,5 @@ public final class ContainerUtil {
     JdbcContainer.create(PG, PG_LOCATIONS),
     JdbcContainer.create(MYSQL, MYSQL_LOCATIONS)
   );
-
-  public static List<DynamicTest> testAllContainers(ContainerExecutable executable) {
-    List<DynamicTest> tests = new ArrayList<>();
-    for (JdbcContainer container : ContainerUtil.containers) {
-      String name = container.getDriverClassName();
-      tests.addAll(
-        executable.execute()
-          .stream()
-          .map(test -> dynamicTest(name + " " + test.name(), () -> {
-            DataSource dataSource = container.start();
-            JDBC.configuration(dataSource);
-            try {
-              test.executable().execute(dataSource);
-            } finally {
-              container.stop();
-            }
-          }))
-          .toList()
-      );
-    }
-    return tests;
-  }
 
 }
