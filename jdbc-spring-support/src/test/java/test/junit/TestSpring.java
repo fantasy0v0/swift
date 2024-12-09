f2def2fe992e13271ad04a4c8c23fe6efaf0d6ae
fan <xiaofanms@outlook.com>
2024-12-09 17:23:43 +0800

    提交修改。

--- jdbc-spring-support/src/test/java/test/junit/TestSpring.java
+++ jdbc-spring-support/src/test/java/test/junit/TestSpring.java
@@ -1,14 +1,69 @@
 package test.junit;
 
+import com.github.fantasy0v0.swift.jdbc.JDBC;
+import com.github.fantasy0v0.swift.jdbc.exception.SwiftException;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
 import test.TestApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import test.service.TestService;
 
+import javax.sql.DataSource;
+
+@Transactional
+@SpringBootTest
 public class TestSpring {
+
+  @Resource
+  private TestService testService;
+
+  @Autowired
+  private PlatformTransactionManager transactionManager;
 
   @Test
   void test() {
-    SpringApplication.run(TestApplication.class);
+    try {
+      JDBC.transaction(() -> {
+        JDBC.update("""
+        update student set name = ? where id = ?
+        """).execute("大明", 1);
+        throw new SwiftException("测试");
+      });
+    } catch (SwiftException e) {
+      // ignore
+    }
+    String name = JDBC.select("""
+    select name from student where id = ?
+    """, 1).fetchOne(row -> row.getString(1));
+    Assertions.assertEquals("小明", name);
+
+    JDBC.transaction(() -> {
+      JDBC.update("""
+      update student set name = ? where id = ?
+      """).execute("大明", 1);
+    });
+    name = JDBC.select("""
+    select name from student where id = ?
+    """, 1).fetchOne(row -> row.getString(1));
+    Assertions.assertEquals("大明", name);
+  }
+
+  @Test
+  void test2() {
+    try {
+      testService.test(true);
+    } catch (SwiftException e) {
+      // ignore
+    }
+    String name = JDBC.select("""
+    select name from student where id = ?
+    """, 1).fetchOne(row -> row.getString(1));
+    Assertions.assertEquals("小明", name);
   }
 
 }
