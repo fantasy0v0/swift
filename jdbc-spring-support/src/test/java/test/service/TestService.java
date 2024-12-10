514d22351d736f6a747b31b54698de170b448870
fan <xiaofanms@outlook.com>
2024-12-10 23:23:31 +0800

    处理事务没有生效的问题。

--- jdbc-spring-support/src/test/java/test/service/TestService.java
+++ jdbc-spring-support/src/test/java/test/service/TestService.java
@@ -2,35 +2,23 @@ package test.service;
 
 import com.github.fantasy0v0.swift.jdbc.JDBC;
 import com.github.fantasy0v0.swift.jdbc.exception.SwiftException;
-import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Assertions;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 @Service
 public class TestService {
 
-  @Resource
-  private DataSource dataSource;
-
-  @Resource
-  private JdbcTemplate jdbcTemplate;
-
   @Transactional
   public void test(boolean throwError) {
-    int result = jdbcTemplate.update(
-      "update student set name = ? where id = ?", "大明", 1);
+    int result = JDBC.update("""
+    update student set name = ? where id = ?
+    """).execute("大明", 1);
     Assertions.assertEquals(1, result);
-//    JDBC.update("""
-//    update student set name = ? where id = ?
-//    """).execute("大明", 1);
+    String name = JDBC.select("""
+    select name from student where id = ?
+    """, 1).fetchOne(row -> row.getString(1));
+    Assertions.assertEquals("大明", name);
     if (throwError) {
       throw new SwiftException("测试");
     }
