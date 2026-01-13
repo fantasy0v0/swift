db0334fcaa102faf8cda9768218995405bb1067d
fan <xiaofanms@outlook.com>
2026-01-13 18:00:16 +0800

    学习事务。

--- core/src/test/java/test/jdbc/TransactionTest.java
+++ core/src/test/java/test/jdbc/TransactionTest.java
@@ -1,5 +1,6 @@
 package test.jdbc;
 
+import com.github.fantasy0v0.swift.exception.SwiftException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,10 +10,7 @@ import test.container.Db;
 import test.container.SwiftJdbcExtension;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 @ExtendWith(SwiftJdbcExtension.class)
 public class TransactionTest {
@@ -21,7 +19,31 @@ public class TransactionTest {
 
   @TestTemplate
   void test(DataSource dataSource, Db db) throws SQLException {
-
+    try (Connection connection = dataSource.getConnection()) {
+      // 这并不算是开启事务
+      connection.setAutoCommit(false);
+      // 事务开启之前可以任意修改隔离级别
+      connection.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED);
+      // 主动发起查询开启事务
+      try (Statement stmt = connection.createStatement()) {
+        try (ResultSet resultSet = stmt.executeQuery("select 1")) {
+          Assertions.assertTrue(resultSet.next());
+        }
+      }
+      int transactionIsolation = connection.getTransactionIsolation();
+      log.debug("transactionIsolation: {}", transactionIsolation);
+      // 开启事务后无法修改
+      if (Db.Postgres == db) {
+        Assertions.assertThrowsExactly(SwiftException.class, () -> {
+          connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+        });
+      } else {
+        // MySQL虽然不报错但是无法修改
+        connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+      }
+      connection.commit();
+      log.debug("autoCommit: {}", connection.getAutoCommit());
+    }
   }
 
   @TestTemplate
